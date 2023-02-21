297ad495c882e487da78a7d031ad8ed682f2b25a
Rodolfo Andrés Rivas Matta <rodolforivasmatta@gmail.com>
2023-02-21 15:44:34 +0000

    Complete basic API.

--- src/metl.h
+++ src/metl.h
@@ -5,15 +5,22 @@
 #define _METL_H_
 
 #include <string>
+#include <chrono>
 
-namespace metl { // Implementation.
+namespace metl {
     class METL {
     public:
         METL(std::string clock_name);
         ~METL(); // Stops the clock if clock hasn't stopped.
-        
+
         METL& start();  // Starts the clock.
         void stop();    // Manually stop the clock (recommended).
+
+        // Stopping the clock makes it print the info into the terminal.
+
+    private:
+        std::chrono::time_point<std::chrono::high_resolution_clock> start;
+        std::chrono::time_point<std::chrono::high_resolution_clock> end;
     }
 }
 
