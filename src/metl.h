de628134c675aae6f8700f801b66900286318f50
Rodolfo Andrés Rivas Matta <rodolforivasmatta@gmail.com>
2023-02-21 18:36:09 +0000

    Adding documentation to the header file.
    Adding documentation and making some small changes for the internal
    implementation.

--- src/metl.h
+++ src/metl.h
@@ -10,15 +10,46 @@
 namespace metl {
     class METL {
     public:
+        /**
+         * Initializes the basic info for the clock, but it doesn't start it.
+         *
+         * @param clock_name String used for printing the clock info.
+         */
         METL(std::string clock_name);
-        ~METL(); // Stops the clock if clock hasn't stopped.
 
-        METL& start();  // Starts the clock.
-        void stop();    // Manually stop the clock (recommended).
+        /**
+         * The destructor just check if the clock was stopped. If it wasn't, it
+         * will issue a warning and will call the {@link METL::stop() stop}
+         * method.
+         *
+         * In the special case when the clock wasn't started, it will also issue
+         * a warning.
+         */
+        ~METL();
 
-        // Stopping the clock makes it print the info into the terminal.
+        /**
+         * Starts running the clock.
+         *
+         * @return A reference to the current object.
+         */
+        METL& start();
+
+        /**
+         * Stops the clock and prints the clock duration in the terminal. If the
+         * clock wasn't started, it will issue a warning.
+         */
+        void stop();
 
     private:
+        /**
+         * Internal method to print the results. The clock must be stopped
+         * before being called.
+         */
+        void log_terminal();
+        
+        std::string name;
+        bool stopped;
+        bool started;
         std::chrono::time_point<std::chrono::high_resolution_clock> start;
         std::chrono::time_point<std::chrono::high_resolution_clock> end;
     }
