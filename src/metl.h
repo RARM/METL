4092edf0ed7c2a7e6db60607c9d62a1f387a0b9b
Rodolfo Andrés Rivas Matta <rodolforivasmatta@gmail.com>
2023-02-25 00:05:49 +0000

    Solving the issue of conditional compiling.
    Completed the header code. Documentation still pending.

--- src/metl.h
+++ src/metl.h
@@ -1,12 +1,41 @@
-// metl.h -- Execution time measuring library.
+/**
+ * @file metl.h
+ * @brief Minimalistic Execution Time Library
+ */
 
-#if defined(NDEBUG) || defined(METLDEBUG)
+/**
+ * @def metl_start_clock(X)
+ * @brief 
+ * @param
+ */
+
+/**
+ * @def metl_stop_clock(X)
+ * @brief 
+ * @param
+ */
+
+/**
+ * @def metl_stop_clock()
+ * @brief 
+ */
+
 #ifndef _METL_H_
 #define _METL_H_
 
+#if defined(NDEBUG) || defined(METLDEBUG)
+
 #include <string>
 #include <chrono>
+#include <list>
 
+/**
+ * @namespace metl
+ * 
+ * METL code available only on non debug environments. It is recommended to use
+ * macros instead of this API, but these could also be used provided the proper
+ * conditional preprocessing is done to avoid errors during code generation.
+ */
 namespace metl {
     class METL {
     public:
@@ -43,7 +72,7 @@ namespace metl {
     private:
         /**
          * Internal method to print the results. The clock must be stopped
-         * before being called.
+         * before calling this function.
          */
         void log_terminal();
         
@@ -53,7 +82,42 @@ namespace metl {
         std::chrono::time_point<std::chrono::high_resolution_clock> start;
         std::chrono::time_point<std::chrono::high_resolution_clock> end;
     }
+
+    class GlobalControl {
+    private:
+        static std::list<metl::METL> clocks;
+    
+    public:
+        /**
+         * Create and save clock with the given name.
+         *
+         * @param name Clock name.
+         */
+        static void start_clock(std::string name);
+
+        /**
+         * Stop the last clock added or a clock by giving the name of it. Note
+         * that giving a name would take longer to stop it. Stop last added
+         * clock 
+         *
+         * @param name Clock name.
+         */
+        static void stop_clock(std::string name = "");
+    }
 }
 
-#endif /* _METL_H_ */
+// Macros for using the library classes if in debug mode.
+
+#define metl_start_clock(X) metl::GlobalControl::start_clock(X)
+#define metl_stop_clock(X)  metl::GlobalControl::stop_clock(X)
+#define metl_stop_clock()   metl::GlobalControl::stop_clock()
+
+#else /* defined(NDEBUG) || defined(METLDEBUG) */
+
+// Macro to replace for no code in no debug environments.
+#define metl_start_clock(X)
+#define metl_stop_clock(X)
+#define metl_stop_clock()
+
 #endif /* defined(NDEBUG) || defined(METLDEBUG) */
+#endif /* _METL_H_ */
